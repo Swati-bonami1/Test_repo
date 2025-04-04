9b4cddcf097bd0da4fed726ff440539cbbabb68b
Swati-bonami1 <swati.kumari@bonamisoftware.com>
2025-04-04 11:18:33 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def square_area():
+  formula = (22/7)*r*r
+  print("Here's the area of circle, " formula)
+
